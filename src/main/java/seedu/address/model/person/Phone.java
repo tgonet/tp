b7fe2fcb67d33a5c8499b2eb4bc69dff1f20c05c
YHKok <yonghsienkok.17@gmail.com>
2025-10-08 13:32:18 +0800

    Update Phone.java

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,7 +11,7 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be 8 digits long";
+            "Phone numbers should only contain numbers, start with 8 or 9 and it should be 8 digits long";
     public static final String VALIDATION_REGEX = "^[89]\\d{7}$";
     public final String value;
 
