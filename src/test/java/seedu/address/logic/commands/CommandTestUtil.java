0d7a01f3d42de97eb2a81ff97e5c020a45db19f4
YHKok <yonghsienkok.17@gmail.com>
2025-10-08 13:32:27 +0800

    Update CommandTestUtil.java

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -32,8 +32,8 @@ public class CommandTestUtil {
     public static final String VALID_PHONE_BOB = "87654321";
     public static final String VALID_EMAIL_AMY = "amy@example.com";
     public static final String VALID_EMAIL_BOB = "bob@example.com";
-    public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
-    public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
+    public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1, Singapore 654321";
+    public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3, Singapore 678901";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
 
