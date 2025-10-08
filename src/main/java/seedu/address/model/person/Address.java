dcdf50ad6a80d93c2a445aef41a5ef904dfeb3c6
tgonet <tanmingzhe.99@gmail.com>
2025-10-08 17:26:38 +0800

    Update for Address.java for accidental removal of method

--- src/main/java/seedu/address/model/person/Address.java
+++ src/main/java/seedu/address/model/person/Address.java
@@ -5,13 +5,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
  * Represents a Person's address in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ * Guarantees: immutable; is valid as declared in
+ * {@link #isValidAddress(String)}
  */
 public class Address {
 
-    public static final String MESSAGE_CONSTRAINTS =
-            "Addresses should be between 20 - 100 characters long and can take any values except for special characters"
-                    + ", and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Addresses should be between 20 - 100 characters long and can take any values except for special characters"
+            + ", and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
@@ -30,6 +30,13 @@ public class Address {
         requireNonNull(address);
         checkArgument(isValidAddress(address), MESSAGE_CONSTRAINTS);
         value = address;
+    }
+
+    /**
+     * Returns true if a given string is a valid email.
+     */
+    public static boolean isValidAddress(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
