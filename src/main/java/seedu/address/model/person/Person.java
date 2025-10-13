71a6364f120fec72599b946f81da066ae3fa59ac
YHKok <yonghsienkok.17@gmail.com>
2025-10-13 17:59:42 +0800

    Update Person.java

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -22,16 +22,18 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private final Remark remark;
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Address address, Remark remark, Set<Tag> tags) {
         requireAllNonNull(name, phone, address, tags);
         this.name = name;
         this.phone = phone;
         this.address = address;
+        this.remark = remark;
         this.tags.addAll(tags);
     }
 
@@ -45,6 +47,10 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+
+    public Remark getRemark() {
+        return remark;
     }
 
     /**
