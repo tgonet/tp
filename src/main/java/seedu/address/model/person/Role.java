255f9a4796616bd5fdaf558fd5663016e8647df6
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2025-10-14 21:49:33 +0800

    Add Role class and test class

---
+++ src/main/java/seedu/address/model/person/Role.java
@@ -0,0 +1,64 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's role in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidRole(String)}
+ */
+public class Role {
+    public static final String MESSAGE_CONSTRAINTS = "Names should only contain" +
+            " alphabetic characters and should not be blank";
+
+    /*
+     * Only allow the role to either be student or parent
+     */
+    public static final String VALIDATION_REGEX = "^(student|parent)$";
+
+    public final String role;
+
+    /**
+     * Constructs a {@code Name}.
+     *
+     * @param role A valid name.
+     */
+    public Role(String role) {
+        requireNonNull(role);
+        role = role.toLowerCase();
+        checkArgument(isValidRole(role), MESSAGE_CONSTRAINTS);
+        this.role = role;
+    }
+
+    /**
+     * Returns true if a given string is a valid role.
+     */
+    public static boolean isValidRole(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return role;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Role)) {
+            return false;
+        }
+
+        Role otherRole = (Role) other;
+        return role.equals(otherRole.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return role.hashCode();
+    }
+}
