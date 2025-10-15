af56d173ac4f2218662d77908e13acffe1696876
tgonet <tanmingzhe.99@gmail.com>
2025-10-15 13:45:03 +0800

    Remove unused imports

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -5,21 +5,14 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.model.person.Phone;
-import seedu.address.model.person.Role;
 import seedu.address.model.person.RoleContainsKeywordsPredicate;
-import seedu.address.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -37,32 +30,27 @@ public class FindCommandParser implements Parser<FindCommand> {
 
         String trimmedArgs = args.trim();
 
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_ROLE, PREFIX_TAG);
+        // ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_ROLE, PREFIX_TAG);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_ROLE);
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_ROLE);
 
-        // Use .getValue() which returns Optional<String>, and handle missing values
-        // Name keywords
         List<String> name = argMultimap.getValue(PREFIX_NAME)
-                .map(value -> Arrays.asList(value.split(","))) // split if present
-                .orElse(Collections.emptyList()); // if not present, return empty list
+                .map(value -> Arrays.asList(value.split(",")))
+                .orElse(Collections.emptyList()); 
 
-        // Role keywords
         List<String> role = argMultimap.getValue(PREFIX_ROLE)
                 .map(value -> Arrays.asList(value.split(",")))
                 .orElse(Collections.emptyList());
 
-        // Tag keywords
-        List<String> tagList = argMultimap.getValue(PREFIX_TAG)
-                .map(value -> Arrays.asList(value.split(",")))
-                .orElse(Collections.emptyList());
+        // List<String> tagList = argMultimap.getValue(PREFIX_TAG)
+        //         .map(value -> Arrays.asList(value.split(",")))
+        //         .orElse(Collections.emptyList());
 
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
-
-        //String[] nameKeywords = trimmedArgs.split("\\s+");
 
         return new FindCommand(new NameContainsKeywordsPredicate(name), new RoleContainsKeywordsPredicate(role));
     }
